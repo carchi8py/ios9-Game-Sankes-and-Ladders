2c1d850bc16b62c2a000e1649727ec39403341cc
Chris Archibald <carchi8py@gmail.com>
2016-01-31 18:01:01 -0800

    Check if a user hit a Snake or Ladder.

--- SnakesAndLadders/GameScene.swift
+++ SnakesAndLadders/GameScene.swift
@@ -74,6 +74,82 @@ class GameScene: SKScene {
         backgoundMusic.play()
     }
     
+    // Check when a player lander on ladder or snake
+    func checkHit(currentPosition currentPosition: Int) -> Bool {
+        var returnValue: Bool = false
+        switch (currentPosition) {
+        case 7, 20, 25, 28, 36, 42, 51, 56, 59, 62, 69, 71, 83, 86, 91, 94, 99:
+            returnValue = true
+            break
+        default:
+            returnValue = false
+            break
+        }
+        return returnValue
+    }
+
+    //finding a new position when hit == true
+    func snakesAndLadders(currentPosition currentPosition: Int) -> Float {
+        var newPosition = -1
+        switch(currentPosition) {
+        case 7:
+            newPosition = 11
+            break
+        case 20:
+            newPosition = 38
+            break
+        case 25:
+            newPosition = 3
+            break
+        case 28:
+            newPosition = 65
+            break
+        case 36:
+            newPosition = 44
+            break
+        case 42:
+            newPosition = 63
+            break
+        case 51:
+            newPosition = 67
+            break
+        case 56:
+            newPosition = 48
+            break
+        case 59:
+            newPosition = 1
+            break
+        case 62:
+            newPosition = 81
+            break
+        case 69:
+            newPosition = 32
+            break
+        case 71:
+            newPosition = 90
+            break
+        case 83:
+            newPosition = 57
+            break
+        case 86:
+            newPosition = 97
+            break
+        case 91:
+            newPosition = 73
+            break
+        case 94:
+            newPosition = 26
+            break
+        case 99:
+            newPosition = 80
+            break
+        default:
+            break
+        }
+        return Float(newPosition)
+    
+    }
+    
     func setupSounds() {
         let url: NSURL = NSBundle.mainBundle().URLForResource("gameplaybackground", withExtension: "mp3")!
         
