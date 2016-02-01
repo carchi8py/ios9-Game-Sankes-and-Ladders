5c137c4b200c8abdad907936faa4879d5987c13b
Chris Archibald <carchi8py@gmail.com>
2016-01-31 16:27:21 -0800

    Set up our default title screen.

--- SnakesAndLadders/GameViewController.swift
+++ SnakesAndLadders/GameViewController.swift
@@ -14,11 +14,11 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameScene") {
+        if let scene = TitleScene(fileNamed:"TitleScene") {
             // Configure the view.
             let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+            skView.showsFPS = false
+            skView.showsNodeCount = false
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
---
+++ SnakesAndLadders/TitleScene.swift
@@ -0,0 +1,14 @@
+//
+//  TitleScene.swift
+//  SnakesAndLadders
+//
+//  Created by Chris Archibald on 1/31/16.
+//  Copyright © 2016 Chris Archibald. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class TitleScene: SKScene {
+
+}
