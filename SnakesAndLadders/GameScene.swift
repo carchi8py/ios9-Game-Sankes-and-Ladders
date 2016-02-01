a9ee71a061ba9aee9ef3a6f3cc91a37fad80ff55
Chris Archibald <carchi8py@gmail.com>
2016-01-31 17:30:49 -0800

    Set Game Variables.

--- SnakesAndLadders/GameScene.swift
+++ SnakesAndLadders/GameScene.swift
@@ -7,8 +7,47 @@
 //
 
 import SpriteKit
+import AVFoundation
 
 class GameScene: SKScene {
+    
+    var board = SKSpriteNode()
+    
+    //Game State Variables
+    var gamePlayInProgrss: Bool = false
+    var player1Turn: Bool = false
+    var rolling: Bool = false
+    var moveFinished: Bool = true
+    var gameOver: Bool = false
+    
+    //Player Positions
+    var player1CurrentPosition: Int = 0
+    var player2CurrentPosition: Int = 0
+    
+    //Rolled Dice
+    var rolledDice: Int = 0
+    
+    //player Sprite Nodes
+    var player1: SKSpriteNode = SKSpriteNode()
+    var player2: SKSpriteNode = SKSpriteNode()
+    
+    //Player Current Position Labels
+    var player1CurrentPositionLabel: SKLabelNode = SKLabelNode()
+    var player2CurrentPositionLabel: SKLabelNode = SKLabelNode()
+    
+    // Status and tap Labels
+    var statusLabel: SKLabelNode = SKLabelNode()
+    var tapLabel: SKLabelNode = SKLabelNode()
+    
+    //Game Sounds
+    var diceSFX: SKAction = SKAction()
+    var snakeSFX: SKAction = SKAction()
+    var ladderSFX: SKAction = SKAction()
+    var backgoundMusic: AVAudioPlayer = AVAudioPlayer()
+    var gameOverMusic: AVAudioPlayer = AVAudioPlayer()
+    
+    
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
     }
